f959f414a5b5f563a420eba0c52b43dcf1833af0
Ruben B <rubsl@hotmail.com>
2024-02-10 20:36:54 -0600

    Fisrt Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#Práctica 1
+
+##Version
+Java 17
+Configuración de JDK a través de Jenv
+
+##Enlace
+[link name]
---
+++ src/main/java/com/lite/practica1/Practica1Application.java
@@ -0,0 +1,14 @@
+package com.lite.practica1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica1Application.class, args);
+		System.out.println("Hola, mi nobre es: Ruben");
+	}
+
+}
